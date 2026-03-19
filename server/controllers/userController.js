c86fcbbabdf8c25b5c69098130cd8902352a8303
khaya05 <johndoe@example.com>
2026-03-20 01:22:03 +0200

    add user controller with getCurrentUser function to retrieve user data

---
+++ server/controllers/userController.js
@@ -0,0 +1,10 @@
+import { asyncWrapper } from "../util/asyncWrapper";
+import User from '../models/userModel.js'
+import { StatusCodes } from "http-status-codes";
+
+export const getCurrentUser = asyncWrapper(async (req, res) => {
+  const user = await User.findById(req.user.userId)
+  const { password: _pw, ...safeUser } = user.toObject();
+
+  res.status(StatusCodes.OK).json({ user: safeUser });
+})
